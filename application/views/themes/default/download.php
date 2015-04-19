5293e83bccc784964e4f0cf4df419a6ed97079ba
zhijie <zhijie@mogujie.com>
2015-04-19 16:57:12 +0800

    improves word

--- application/views/themes/default/download.php
+++ application/views/themes/default/download.php
@@ -1,63 +1,71 @@
 <section id="content">
     <?php p($subMenu); ?>
     <div class="container">
-        <div class="m-t-xl m-b-xl text-center"><h2>Find the Right Pricing Plan For You</h2></div>
+        <div class="m-t-xl m-b-xl text-center">
+			<h2>选择合适您的版本</h2>
+		</div>
         <div class="clearfix">
             <div class="row m-b-xl">
                 <div class="col-sm-4 animated fadeInLeftBig">
                     <section class="panel b-light text-center m-t">
-                        <header class="panel-heading bg-white b-light"><h3 class="m-t-sm">Personal</h3>
-
-                            <p>for freelancers and start ups</p></header>
+                        <header class="panel-heading bg-white b-light">
+							<h3 class="m-t-sm">个人版</h3>
+                            <p>个人站长或公益组织类</p>
+						</header>
                         <ul class="list-group">
-                            <li class="list-group-item text-center bg-light lt"><span class="text-danger font-bold h1">$20</span>
-                                / month
+                            <li class="list-group-item text-center bg-light lt">
+								<span class="text-danger font-bold h1">0</span> / 永久免费
                             </li>
-                            <li class="list-group-item"> Max 50 projects</li>
-                            <li class="list-group-item"> Max 500 tasks</li>
-                            <li class="list-group-item"> Max 2,000 users</li>
-                            <li class="list-group-item"> Upgrade for next update</li>
-                            <li class="list-group-item"> 7*24 free support</li>
+                            <li class="list-group-item"> 全功能版本</li>
+                            <li class="list-group-item"> 免费一个主题</li>
+                            <li class="list-group-item"> 免费一个模块</li>
+                            <li class="list-group-item"> 手动免费升级后续版本</li>
+                            <li class="list-group-item"> 无技术支持</li>
                         </ul>
-                        <footer class="panel-footer"><a href="#" class="btn btn-primary m-t m-b">Get Started</a>
+                        <footer class="panel-footer">
+							<a href="#" class="btn btn-primary m-t m-b">在线试用</a>
                         </footer>
                     </section>
                 </div>
                 <div class="col-sm-4 animated fadeInUp">
                     <section class="panel b-primary text-center">
-                        <header class="panel-heading bg-primary"><h3 class="m-t-sm">Business</h3>
-
-                            <p>for businesses and organizations</p></header>
+                        <header class="panel-heading bg-primary">
+							<h3 class="m-t-sm">商业版</h3>
+                            <p>公司、政府、学校等组织</p>
+						</header>
                         <ul class="list-group">
                             <li class="list-group-item text-center bg-light lt">
-                                <div class="padder-v"><span class="text-danger font-bold h1">$50</span> / month</div>
+                                <div class="padder-v"><span class="text-danger font-bold h1">￥3000</span> / 年</div>
                             </li>
-                            <li class="list-group-item"> Max 500 projects</li>
-                            <li class="list-group-item"> Max 5,000 tasks</li>
-                            <li class="list-group-item"> Max 20,000 users</li>
-                            <li class="list-group-item"> Upgrade for next update</li>
-                            <li class="list-group-item"> 7*24 free support</li>
+                            <li class="list-group-item"> 全功能版本</li>
+                            <li class="list-group-item"> 所有主题免费</li>
+                            <li class="list-group-item"> 所有模块免费</li>
+                            <li class="list-group-item"> 主动免费升级后续版本</li>
+                            <li class="list-group-item"> 7*24 小时技术支持</li>
                         </ul>
-                        <footer class="panel-footer"><a href="#" class="btn btn-primary m-t m-b">Get Started</a>
+                        <footer class="panel-footer">
+							<a href="#" class="btn btn-primary m-t m-b">在线试用</a>
                         </footer>
                     </section>
                 </div>
                 <div class="col-sm-4 animated fadeInRightBig">
                     <section class="panel b-light text-center m-t">
-                        <header class="panel-heading bg-white b-light"><h3 class="m-t-sm">Enterprise</h3>
-
-                            <p>for enterprise and companies</p></header>
+                        <header class="panel-heading bg-white b-light">
+							<h3 class="m-t-sm">定制版</h3>
+                            <p>特殊需求用户</p>
+						</header>
                         <ul class="list-group">
-                            <li class="list-group-item text-center bg-light lt"><span class="text-danger font-bold h1">$100</span>
-                                / month
+                            <li class="list-group-item text-center bg-light lt"><span class="text-danger font-bold h1"> ？</span>
+                                / 联系商谈
                             </li>
-                            <li class="list-group-item"> Unlimited projects</li>
-                            <li class="list-group-item"> Unlimited tasks</li>
+                            <li class="list-group-item"> 根据业务需求进行定制系统</li>
+                            <li class="list-group-item"> 定制专属网站模板主题</li>
                             <li class="list-group-item"> Unlimited users</li>
-                            <li class="list-group-item"> Upgrade for next update</li>
-                            <li class="list-group-item"> 7*24 free support</li>
+                            <li class="list-group-item"> 主动免费升级后续版本</li>
+                            <li class="list-group-item"> 7*24 小时技术支持</li>
                         </ul>
-                        <footer class="panel-footer"><a href="#" class="btn btn-primary m-t m-b">Get Started</a>
+                        <footer class="panel-footer">
+							<a href="#" class="btn btn-primary m-t m-b">在线试用</a>
                         </footer>
                     </section>
                 </div>
@@ -66,39 +74,8 @@
     </div>
     <div class="bg-white-only">
         <div class="container">
-            <div class="m-t-xl m-b-xl"><h2 class="font-thin">Why more than 21,000 project owners have chosen us</h2>
-            </div>
-            <div class="row m-b-xl">
-                <div class="col-sm-4" data-ride="animated" data-animation="fadeInLeft" data-delay="300"><span
-                        class="fa-stack fa-2x pull-left m-r m-t-xs"> <i class="fa fa-circle fa-stack-2x text-info"></i> <i
-                        class="fa fa-stack-1x fa-inverse">1</i> </span>
-
-                    <div class="clear"><h4>Lightweight with Rich Components</h4>
-
-                        <p class="text-muted text-sm">We only choose the lightweight plugins, but with rich functions.
-                            we use tiny components, css and js size is first consideration. <br><br>This project ues
-                            HTML5 and CSS3 to build the components and widgets. the pages use header, nav, section to
-                            build the layout.</p></div>
-                </div>
-                <div class="col-sm-4" data-ride="animated" data-animation="fadeInLeft" data-delay="600"><span
-                        class="fa-stack fa-2x pull-left m-r m-t-xs"> <i class="fa fa-circle fa-stack-2x text-info"></i> <i
-                        class="fa fa-stack-1x fa-inverse">2</i> </span>
-
-                    <div class="clear"><h4>Keep Updating with Great Support</h4>
-
-                        <p class="text-muted text-sm">This project is based on Bootstrap 3. the sleek, intuitive, and
-                            powerful mobile-first front-end framework for faster and easier web development. we will
-                            keep it updated to Bootstrap.<br><br> <a href="#" class="text-primary">More...</a></p></div>
-                </div>
-                <div class="col-sm-4" data-ride="animated" data-animation="fadeInLeft" data-delay="900"><span
-                        class="fa-stack fa-2x pull-left m-r m-t-xs"> <i class="fa fa-circle fa-stack-2x text-info"></i> <i
-                        class="fa fa-stack-1x fa-inverse">3</i> </span>
-
-                    <div class="clear"><h4>HTML5 & CSS3 with Clean Design</h4>
-
-                        <p class="text-muted text-sm">This project ues HTML5 and CSS3 to build the components and
-                            widgets. the pages use header, nav, section to build the layout. </p></div>
-                </div>
+            <div class="m-t-xl m-b-xl">
+				<h2 class="font-thin">已经有 21,000 个网站在使用简站(Simple-Site)搭建营销系统！</h2>
             </div>
         </div>
     </div>
--- application/views/themes/default/news.php
+++ application/views/themes/default/news.php
@@ -79,125 +79,28 @@
                         <li><a href="#"><i class="fa fa-chevron-right"></i></a></li>
                     </ul>
                 </div>
-                <h4 class="m-t-lg m-b">3 Comments</h4>
-                <section class="comment-list block">
-                    <article id="comment-id-1" class="comment-item"><a class="pull-left thumb-sm"> <img
-                            src="images/avatar.jpg" class="img-rounded"> </a>
-                        <section class="comment-body m-b">
-                            <header><a href="#"><strong>John smith</strong></a> <label class="label bg-info m-l-xs">Editor</label>
-                                <span class="text-muted text-xs block m-t-xs"> 24 minutes ago </span></header>
-                            <div class="m-t-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi id neque
-                                quam. Aliquam sollicitudin venenatis ipsum ac feugiat. Vestibulum.
-                            </div>
-                        </section>
-                    </article>
-                    <!-- .comment-reply -->
-                    <article id="comment-id-2" class="comment-item comment-reply"><a class="pull-left thumb-sm"> <img
-                            src="images/avatar.jpg" class="img-rounded"> </a>
-                        <section class="comment-body m-b">
-                            <header><a href="#"><strong>John smith</strong></a> <label class="label bg-dark m-l-xs">Admin</label>
-                                <span class="text-muted text-xs block m-t-xs"> 26 minutes ago </span></header>
-                            <div class="m-t-sm">Lorem ipsum dolor sit amet, consecteter adipiscing elit, sed diam
-                                nonummy nibh euismod tincidunt ut laoreet.
-                            </div>
-                        </section>
-                    </article>
-                    <!-- / .comment-reply -->
-                    <article id="comment-id-2" class="comment-item"><a class="pull-left thumb-sm"> <img
-                            src="images/avatar.jpg" class="img-rounded"> </a>
-                        <section class="comment-body m-b">
-                            <header><a href="#"><strong>John smith</strong></a> <label class="label bg-dark m-l-xs">Admin</label>
-                                <span class="text-muted text-xs block m-t-xs"> 26 minutes ago </span></header>
-                            <blockquote class="m-t"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-                                posuere erat a ante.</p>
-                                <small>Someone famous in <cite title="Source Title">Source Title</cite></small>
-                            </blockquote>
-                            <div class="m-t-sm">Lorem ipsum dolor sit amet, consecteter adipiscing elit, sed diam
-                                nonummy nibh euismod tincidunt ut laoreet.
-                            </div>
-                        </section>
-                    </article>
-                </section>
-                <h4 class="m-t-lg m-b">Leave a comment</h4>
-
-                <form>
-                    <div class="form-group pull-in clearfix">
-                        <div class="col-sm-6"><label>Your name</label> <input type="text" class="form-control"
-                                                                              placeholder="Name"></div>
-                        <div class="col-sm-6"><label>Email</label> <input type="email" class="form-control"
-                                                                          placeholder="Enter email"></div>
-                    </div>
-                    <div class="form-group"><label>Comment</label> <textarea class="form-control" rows="5"
-                                                                             placeholder="Type your comment"></textarea>
-                    </div>
-                    <div class="form-group">
-                        <button type="submit" class="btn btn-success">Submit comment</button>
-                    </div>
-                </form>
             </div>
-            <div class="col-sm-3"><h5 class="font-semibold">About me</h5>
-
-                <div class="line line-dashed"></div>
-                <div class="m-b-lg"><span class="pull-left thumb-sm avatar m-r-sm m-t-xs"> <img src="images/avatar.jpg"
-                                                                                                class="img-circle"> </span>
-
-                    <p class="clear text-sm"> I am a UI/UX designer for Flatfull where i focus on Web applications,
-                        Mobile apps... </p></div>
-                <h5 class="font-semibold">Subscribe</h5>
-
-                <div class="line line-dashed"></div>
-                <div class="m-t-sm m-b-lg"><a href="#" title="RSS" class="btn btn-rounded btn-warning btn-icon"><i
-                        class="fa fa-rss"></i></a> <a href="#" title="Twitter"
-                                                      class="btn btn-rounded btn-twitter btn-icon"><i
-                        class="fa fa-twitter"></i></a> <a href="#" title="Facebook"
-                                                          class="btn btn-rounded btn-facebook btn-icon"><i
-                        class="fa fa-facebook"></i></a> <a href="#" title="Google+"
-                                                           class="btn btn-rounded btn-gplus btn-icon"><i
-                        class="fa fa-google-plus"></i></a> <a href="#" title="LinkedIn"
-                                                              class="btn btn-rounded btn-info btn-icon"><i
-                        class="fa fa-linkedin"></i></a></div>
-                <h5 class="font-semibold">Recent Tweets</h5>
-
-                <div class="line line-dashed"></div>
-                <div class="clear m-b"><a href="#" class="text-info"> <span class="thumb-sm"> <img
-                        src="images/avatar.jpg" class="img-circle"> </span> @Mike Mcalidek <i class="fa fa-twitter"></i>
-                </a></div>
-                <ul class="list-unstyled m-b-lg">
-                    <li><p>Wellcome <a href="#" class="text-info">@Drew Wllon</a> and play this web application
-                        template, have fun1 </p>
-                        <small class="block text-muted"><i class="fa fa-clock-o"></i> 2 minuts ago</small>
-                        <div class="line"></div>
-                    </li>
-                    <li><p>Morbi nec <a href="#" class="text-info">@Jonathan George</a> nunc condimentum ipsum dolor sit
-                        amet, consectetur</p>
-                        <small class="block text-muted"><i class="fa fa-clock-o"></i> 1 hour ago</small>
-                        <div class="line"></div>
-                    </li>
-                    <li><p><a href="#" class="text-info">@Josh Long</a> Vestibulum ullamcorper sodales nisi nec
-                        adipiscing elit. Morbi id neque quam. Aliquam sollicitudin venenatis</p>
-                        <small class="block text-muted"><i class="fa fa-clock-o"></i> 2 hours ago</small>
-                        <div class="line"></div>
-                    </li>
-                </ul>
-                <h5 class="font-semibold">Categories</h5>
+            <div class="col-sm-3">
+                <h5 class="font-semibold">栏目分类</h5>
 
                 <div class="line line-dashed"></div>
                 <ul class="list-unstyled">
-                    <li><a href="#" class="dk"> <span class="badge pull-right">15</span> Photograph </a></li>
+                    <li><a href="#" class="dk"> <span class="badge pull-right">15</span> 新闻公告 </a></li>
                     <li class="line"></li>
-                    <li><a href="#"> <span class="badge pull-right">30</span> Life style </a></li>
+                    <li><a href="#"> <span class="badge pull-right">30</span> 行业动态 </a></li>
                     <li class="line"></li>
-                    <li><a href="#"> <span class="badge pull-right">9</span> Food </a></li>
-                    <li class="line"></li>
-                    <li><a href="#"> <span class="badge pull-right">4</span> Travel world </a></li>
+                    <li><a href="#"> <span class="badge pull-right">9</span> 升级通知 </a></li>
                     <li class="line"></li>
                 </ul>
-                <div class="tags m-b-lg"><a href="#" class="label bg-success">Bootstrap</a> <a href="#"
-                                                                                               class="label bg-success">Application</a>
-                    <a href="#" class="label bg-success">Apple</a> <a href="#" class="label bg-success">Less</a> <a
-                            href="#" class="label bg-success">Theme</a> <a href="#"
-                                                                           class="label bg-success">Wordpress</a></div>
-                <h5 class="font-semibold">Recent Posts</h5>
+                <div class="tags m-b-lg">
+					<a href="#" class="label bg-success">Bootstrap</a>
+					<a href="#" class="label bg-success">Application</a>
+                    <a href="#" class="label bg-success">Apple</a>
+					<a href="#" class="label bg-success">Less</a>
+					<a href="#" class="label bg-success">Theme</a>
+					<a href="#" class="label bg-success">Wordpress</a>
+				</div>
+                <h5 class="font-semibold">最热文章</h5>
 
                 <div class="line line-dashed"></div>
                 <div>
@@ -210,12 +113,13 @@
                         </div>
                     </article>
                     <div class="line"></div>
-                    <article class="media m-t-none"><a class="pull-left thumb thumb-wrapper m-t-xs"> <img
-                            src="images/thumb_2.png"> </a>
-
-                        <div class="media-body"><a href="#" class="font-semibold">Lorem ipsum dolor sit amet,
+                    <article class="media m-t-none">
+						<a class="pull-left thumb thumb-wrapper m-t-xs">
+							<img src="images/thumb_2.png">
+						</a>
+                        <div class="media-body">
+							<a href="#" class="font-semibold">Lorem ipsum dolor sit amet,
                             consectetur adipiscing elit.</a>
-
                             <div class="text-xs block m-t-xs"><a href="#">Design</a> 2 hours ago</div>
                         </div>
                     </article>
