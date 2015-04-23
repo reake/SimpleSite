0742e4502ba54a084fdeedb97d9358a2877f61aa
zhijie <zhijie@mogujie.com>
2015-04-24 01:21:11 +0800

    crumb

--- application/classes/controller/manage/template.php
+++ application/classes/controller/manage/template.php
@@ -9,6 +9,8 @@ class Controller_Manage_Template extends Controller_Template
 	protected $data = array();
 	protected $user = array();
 	protected $siteId;
+	private $category = array();
+	private $route = '';
 
 	public function before()
 	{
@@ -30,37 +32,44 @@ class Controller_Manage_Template extends Controller_Template
 			$category = array(
 				0 => array(
 					'name' => '预览中心',
+					'description' => ' ',
 					'icon' => 'dashboard',
-					'url' => URL::site('/manage/dashboard/index'),
+					'url' => '',
 					'subCategory' => array(
-						0 => array('name' => '预览中心','route' => 'Manage_Dashboard::index', 'url' => URL::site('/manage/dashboard/index')),
+						0 => array('name' => '预览中心','description' => ' ','route' => 'Manage_Dashboard::index', 'url' => URL::site('/manage/dashboard/index')),
 					)
 				),
 				1 => array(
 					'name' => '内容管理',
+					'description' => ' ',
 					'icon' => 'file-text',
+					'url' => '',
 					'subCategory' => array(
-						0 => array('name' => '网站管理','route' => 'Manage_Content::index', 'url' => URL::site('/manage/content/index')),
-						1 => array('name' => '微信管理','route' => 'index', 'url' => URL::site('/manage/wx/index')),
-						2 => array('name' => '报名管理','route' => 'index', 'url' => URL::site('/manage/su/index')),
-						3 => array('name' => '订单管理','route' => 'index', 'url' => URL::site('/manage/order/index')),
-						4 => array('name' => '广告管理','route' => 'index', 'url' => URL::site('/manage/ad/index')),
-						5 => array('name' => '友情链接','route' => 'index', 'url' => URL::site('/manage/fl/index')),
-						6 => array('name' => '文件管理','route' => 'index', 'url' => URL::site('/manage/file/index'))
+						0 => array('name' => '网站管理','description' => ' ','route' => 'Manage_Content::index', 'url' => URL::site('/manage/content/index')),
+						1 => array('name' => '微信管理','description' => ' ','route' => 'index', 'url' => URL::site('/manage/wx/index')),
+						2 => array('name' => '报名管理','description' => ' ','route' => 'index', 'url' => URL::site('/manage/su/index')),
+						3 => array('name' => '订单管理','description' => ' ','route' => 'index', 'url' => URL::site('/manage/order/index')),
+						4 => array('name' => '广告管理','description' => ' ','route' => 'index', 'url' => URL::site('/manage/ad/index')),
+						5 => array('name' => '友情链接','description' => ' ','route' => 'index', 'url' => URL::site('/manage/fl/index')),
+						6 => array('name' => '文件管理','description' => ' ','route' => 'index', 'url' => URL::site('/manage/file/index'))
 					)
 				),
 				2 => array(
 					'name' => '营销管理',
+					'description' => ' ',
 					'icon' => 'envelope-o',
+					'url' => '',
 					'subCategory' => array(
-						0 => array('name' => '消息管理','route' => 'index', 'url' => URL::site('/manage/site/index')),
-						1 => array('name' => '评论留言','route' => 'index', 'url' => URL::site('/manage/site/index')),
-						2 => array('name' => '用户反馈','route' => 'index', 'url' => URL::site('/manage/site/index'))
+						0 => array('name' => '消息管理','description' => ' ','route' => 'index', 'url' => URL::site('/manage/site/index')),
+						1 => array('name' => '评论留言','description' => ' ','route' => 'index', 'url' => URL::site('/manage/site/index')),
+						2 => array('name' => '用户反馈','description' => ' ','route' => 'index', 'url' => URL::site('/manage/site/index'))
 					)
 				),
 				3 => array(
 					'name' => '系统设置',
+					'description' => ' ',
 					'icon' => 'cog',
+					'url' => ' ',
 					'subCategory' => array(
 						0 => array(
 							'name' => '网站设置',
@@ -75,7 +84,12 @@ class Controller_Manage_Template extends Controller_Template
 								)
 							)
 						),
-						1 => array('name' => '个人设置','route' => 'index', 'url' => URL::site('/manage/profile/index'))
+						1 => array(
+							'name' => '个人设置',
+							'description' => ' ',
+							'route' => 'index',
+							'url' => URL::site('/manage/profile/index')
+						)
 					)
 				)
 			);
@@ -95,16 +109,20 @@ class Controller_Manage_Template extends Controller_Template
 				}
 			}
 
+			$this->category = $category;
+			$this->route = $this->request->controller().'::'.$this->request->action();
+
+
 			$data = array(
 				'siteTitle'  => '简站(Simple-Site)',
 				'keywords'   => '免费建站、微信网站、免费微信网站',
 				'siteUrl'    => 'http://www.simple-site.cn',
 				'copyright'  => 'Copyright © 2015 Simple-Site. All Rights Reserved',
 				'user'       => $this->user,
-				'route' => $this->request->controller().'::'.$this->request->action(),
+				'route' => $this->route,
 				'controller' => $this->request->controller(),
 				'action'     => $this->request->action(),
-				'category' => $category
+				'category' => $this->category,
 			);
 			foreach ($data as $key => $value) View::bind_global($key, $data[$key]);
 		} else {
@@ -114,6 +132,27 @@ class Controller_Manage_Template extends Controller_Template
 
 	public function after()
 	{
+		$crumb = array();
+		foreach ($this->category as $k => $v) {
+			if (in_array($this->route, $v['route'])) {
+				$crumb[] = array('name' => $v['name'], 'description' => $v['description'], 'url' => $v['url']);
+				if(isset($v['subCategory'])){
+					foreach ($v['subCategory'] as $k2 => $c) {
+						if (in_array($this->route, $c['subRoute'])) {
+							$crumb[] = array('name' => $c['name'], 'description' => $c['description'], 'url' => $c['url']);
+							if(isset($c['subCategory'])){
+								foreach ($c['subCategory'] as $k3 => $d) {
+									if ($this->route == $d['route']) {
+										$crumb[] = array('name' => $d['name'], 'description' => $d['description'], 'url' => $d['url']);
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		$this->data['crumb'] = View::factory('manage/crumb', array('crumbs' => $crumb));
 		$view          = View::factory('manage/base');
 		$sites         = ORM::factory('Site')->getAll($this->user['id']);
 		$view->sidebar = View::factory('manage/sidebar', array('sites' => $sites, 'siteId' => $this->siteId));
--- application/views/manage/site/index.php
+++ application/views/manage/site/index.php
@@ -1,11 +1,5 @@
 <section class="scrollable padder">
-	<ul class="breadcrumb no-border no-radius b-b b-light pull-in">
-		<li><a href="<?php p($siteUrl); ?>/_assets/index.html"><i class="fa fa-home"></i> 简站(Simple-Site) </a></li>
-		<li class="active">网站管理</li>
-	</ul>
-	<div class="m-b-md"><h3 class="m-b-none">网站管理</h3>
-		<small>您可以在这里管理您所有的网站！</small>
-	</div>
+	<?php p($crumb); ?>
 	<div class="row">
 		<?php foreach ($sites as $v) { ?>
 		<div class="col-sm-4">
