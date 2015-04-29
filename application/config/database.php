26f91b3170afa50e85a50f44856d251d9cd44e54
zhijie <zhijie@mogujie.com>
2015-04-30 01:13:11 +0800

    Improves

--- application/config/database.php
+++ application/config/database.php
@@ -18,32 +18,32 @@ return array
 			 */
 			'dsn'        => 'mysql:host=localhost;dbname=simple_site',
 			'username'   => 'root',
-			'password'   => '133285',
+			'password'   => '133285.+/*-',
 			'persistent' => FALSE,
 		),
 		'table_prefix' => '',
 		'charset'      => 'utf8',
 		'caching'      => FALSE,
 	),
-	'alternate' => array(
+	'offline'   => array
+	(
 		'type'         => 'PDO',
 		'connection'   => array(
 			/**
-			 * The following options are available for PDO:
-			 * string   dsn         Data Source Name
-			 * string   username    database username
-			 * string   password    database password
-			 * boolean  persistent  use persistent connections?
+			 * The following options are available for MySQL:
+			 * string   hostname     server hostname, or socket
+			 * string   database     database name
+			 * string   username     database username
+			 * string   password     database password
+			 * boolean  persistent   use persistent connections?
+			 * array    variables    system variables as "key => value" pairs
+			 * Ports and sockets may be appended to the hostname.
 			 */
-			'dsn'        => 'mysql:host=localhost;dbname=kohana',
+			'dsn'        => 'mysql:host=localhost;dbname=simple_site',
 			'username'   => 'root',
-			'password'   => 'r00tdb',
+			'password'   => '133285',
 			'persistent' => FALSE,
 		),
-		/**
-		 * The following extra options are available for PDO:
-		 * string   identifier  set the escaping identifier
-		 */
 		'table_prefix' => '',
 		'charset'      => 'utf8',
 		'caching'      => FALSE,
