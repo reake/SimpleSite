454b4ae0ce57262bf0a7305f9685f86a3918c7f7
syrdxb <syrdxb@126.com>
2015-04-30 21:59:31 +0800

    fix keywords && description

--- application/views/themes/default/index.php
+++ application/views/themes/default/index.php
@@ -3,7 +3,8 @@
 <head>
 	<meta charset="utf-8"/>
 	<title><?= $title ?></title>
-	<meta name="description" content="<?= $keywords ?>"/>
+    <meta name="keywords" content="<?= $keywords ?>"/>
+	<meta name="description" content="<?= $description ?>"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
     <!-- Favicon -->
     <link rel="shortcut icon" href="<?= $url ?>/_assets/images/favicon.ico" type="image/x-icon" />
