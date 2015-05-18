7aa3e9547f08667cdee60930976409d241609022
zhijie <zhijie@mogujie.com>
2015-05-18 17:48:49 +0800

    PHPUnittest

--- application/tests/classes/SampleTest.php
+++ application/tests/classes/SampleTest.php
@@ -1,13 +1,13 @@
 <?php defined('SYSPATH') or die('No direct access allowed!');
- 
-class SampleTest extends Kohana_Unittest_TestCase
+
+class SampleTest extends Unittest_TestCase
 {
-    public function testAdd()
-    {
-	$post = array();
-	$response = Request::factory('manage/category/add')->method(Request::POST)->post($post);
- 
-	print_r($response);
-        $this->assertEquals($username, null);
-    }
+	public function testAdd()
+	{
+		$post     = array();
+		$response = Request::factory('http://www.mogujie.com/trade/cart/shoptopcart?isnew=1')->method(Request::POST)->post($post)->execute();
+
+		print_r($response->body());
+		$this->assertTrue(TRUE);
+	}
 }
