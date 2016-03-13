ce6bccef3c5a0d6208d95d12e3733f239f85e2fb
zhijie <zhijie@mogujie.com>
2016-03-13 16:54:27 +0800

    init

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # SimpleSite
 SimpleSite is website content system
 
+# Install
+
+```
+composer install
+```
+
+# Run
+```
+php artisan serve --port=1234
+```
+
+# Log
+```
+tail -f storage/logs/laravel.log
+```
--- application/views/manage/crumb.php
+++
@@ -1,9 +0,0 @@
-<ul class="breadcrumb no-border no-radius b-b b-light pull-in">
-	<li><a href="<?php p($siteUrl); ?>"><i class="fa fa-home"></i> 简站(Simple-Site) </a></li>
-	<?php foreach($crumbs as $v){ ?>
-	<li><a href="<?php p($v['url']); ?>"><?php p($v['name']); ?></a></li>
-	<?php } ?>
-</ul>
-<div class="m-b-md"><h3 class="m-b-none"><?php p(end($crumbs)['name']); ?></h3>
-	<small><?php p(end($crumbs)['description']); ?></small>
-</div>
