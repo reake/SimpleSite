487580aaa690ed6d775a5f850aeb2679a5059d66
zhijie <zhijie@mogujie.com>
2016-03-31 15:37:34 +0800

    add Models

---
+++ app/Model/Feedback.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Feedback extends Model
+{
+    // Table
+    protected $table = 'Feedback';
+}
---
+++ app/Model/Sites.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Sites extends Model
+{
+    // Table
+    protected $table = 'Sites';
+}
